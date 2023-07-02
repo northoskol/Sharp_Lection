471a8abc2148b527651c6229f05e1d828becd3f2
Dmitry <northoskol@gmail.com>
2023-07-02 15:15:33 +0300

    add if else in ex005

---
+++ ex005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username=Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("GREAT it is MASHA");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
