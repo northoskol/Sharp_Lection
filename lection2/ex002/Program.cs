a07cf2ca9f8f22bbff26ca6abca1a8656ece4c30
Dmitry <northoskol@gmail.com>
2023-07-06 23:50:27 +0300

    hello massive!

---
+++ lection2/ex002/Program.cs
@@ -0,0 +1,29 @@
+﻿//Массив
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+            //  0   1   2   3   4   5   6   7   8
+int[] array = { 11, 12, 13, 14, 195, 16, 17, 18, 19 };
+// array[0] = 12;
+// Console.WriteLine(array[4]);
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(result);
+
+
+// int max = Max(
+//     Max(a1, b1, c1),
+//     Max(a2, b2, c2),
+//     Max(a3, b3, c3));
+
+
+// Console.WriteLine(max);
---
+++ lection2/ex003/Program.cs
@@ -0,0 +1,17 @@
+﻿
+            //  0   1   2  3   4   5   6   7  8
+int[] array = { 1, 12, 13, 4, 18, 15, 16, 17, 18 };
+int n = array.Length;
+int find=18;
+int index=0;
+
+while (index<n)
+{
+        if(array[index]==find)
+        {
+            Console.WriteLine(index);
+            break;
+        }
+        // index=index+1;
+        index++;
+}
